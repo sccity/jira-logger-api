c1f8d7164f0ec73f6df5cf9fb531a8253e1af2ef
Lance <lhaynie@sccity.org>
2024-03-16 17:13:29 -0600

    init

---
+++ app.py
@@ -0,0 +1,154 @@
+# **********************************************************
+# * CATEGORY  SOFTWARE
+# * GROUP     ADMIN
+# * AUTHOR    LANCE HAYNIE <LHAYNIE@SCCITY.ORG>
+# **********************************************************
+# Jira Error Logging
+# Copyright Santa Clara City
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.#
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os, requests, json, time, yaml
+from flask import Flask, jsonify, request, abort
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Logger(Resource):
+    def get(self):
+        args = request.args
+        app = args.get("app", default="", type=str)
+        level = args.get("level", default="", type=str)
+        function = args.get("function", default="", type=str)
+        msg = args.get("msg", default="", type=str)
+
+        client = JiraClient(jira_api, jira_auth)
+
+        if app == "":
+            abort(400, description="No app provided!")
+
+        if level == "":
+            abort(400, description="No level provided!")
+
+        if function == "":
+            abort(400, description="No function provided!")
+
+        if msg == "":
+            abort(400, description="No msg provided!")
+
+        return client.create_issue(app, level, function, msg)
+
+
+class JiraClient:
+    def __init__(self, api_url, auth):
+        self.api_url = api_url
+        self.auth = auth
+
+    def fetch_issues(self):
+        headers = {"Accept": "application/json"}
+        query = {"jql": config["jira"]["jql"]}
+        response = requests.get(
+            self.api_url + "/rest/api/3/search",
+            headers=headers,
+            params=query,
+            auth=self.auth,
+        )
+
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            return data.get("issues", [])
+        else:
+            print(f"Failed to fetch Jira issues. Status code: {response.status_code}")
+            return []
+
+    def create_issue(self, app, level, function, msg):
+        existing_issues = self.fetch_issues()
+        new_issue_summary = f"APP: {app} - LEVEL: {level} - FUNCTION: {function}"
+        summary_exists = any(
+            issue["fields"]["summary"] == new_issue_summary for issue in existing_issues
+        )
+
+        if level.lower() == "info":
+            priority = "Lowest"
+        elif level.lower() == "debug":
+            priority = "Low"
+        elif level.lower() == "warning":
+            priority = "Medium"
+        elif level.lower() == "error":
+            priority = "High"
+        elif level.lower() == "critical":
+            priority = "Highest"
+        else:
+            priority = "Medium"
+
+        if not summary_exists:
+            headers = {"Accept": "application/json", "Content-Type": "application/json"}
+            payload = json.dumps(
+                {
+                    "fields": {
+                        "description": {
+                            "content": [
+                                {
+                                    "content": [{"text": f"{msg}", "type": "text"}],
+                                    "type": "paragraph",
+                                }
+                            ],
+                            "type": "doc",
+                            "version": 1,
+                        },
+                        "issuetype": {"name": "Bug"},
+                        "labels": ["ERROR", "JIRA-LOGGER"],
+                        "priority": {"name": priority},
+                        "project": {"key": config["jira"]["project"]},
+                        "components": [{"name": config["jira"]["component"]}],
+                        "summary": new_issue_summary,
+                    }
+                }
+            )
+            response = requests.request(
+                "POST",
+                self.api_url + "/rest/api/3/issue",
+                data=payload,
+                headers=headers,
+                auth=self.auth,
+            )
+
+            return json.loads(response.text)
+        else:
+            return jsonify(msg="An issue with the same summary already exists.")
+
+
+@app.errorhandler(404)
+def PageNotFound(e):
+    return jsonify(error=str(e)), 404
+
+
+@app.route("/", methods=["GET"])
+def HttpRoot():
+    return jsonify(
+        application="Jira Issue Logger",
+        copyright="Santa Clara City (UT)",
+        author="Lance Haynie",
+    )
+
+
+api.add_resource(Logger, "/logger")
+
+if __name__ == "__main__":
+    from waitress import serve
+
+    with open("config/config.yaml", "r") as config_file:
+        config = yaml.safe_load(config_file)
+
+    jira_api = config["jira"]["api"]
+    jira_auth = (config["jira"]["user"], config["jira"]["api_key"])
+
+    serve(app, host="0.0.0.0", port=5000, threads=100)
---
+++ app.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+docker_compose="docker-compose -f docker-compose.yaml"
+
+[ -f config/config.yaml ] || { echo "Missing config.yaml file. Exiting."; exit 1; }
+
+if [[ $1 = "start" ]]; then
+  echo "Starting Jira Logger API..."
+	$docker_compose up -d
+elif [[ $1 = "stop" ]]; then
+	echo "Stopping Jira Logger API..."
+	$docker_compose stop
+elif [[ $1 = "restart" ]]; then
+	echo "Restarting Jira Logger API..."
+  $docker_compose down
+  $docker_compose start
+elif [[ $1 = "down" ]]; then
+	echo "Tearing Down Jira Logger API..."
+	$docker_compose down
+elif [[ $1 = "rebuild" ]]; then
+	echo "Rebuilding Jira Logger API..."
+	$docker_compose down --remove-orphans
+	$docker_compose build --no-cache
+elif [[ $1 = "update" ]]; then
+	echo "Updating Jira Logger API..."
+	$docker_compose down --remove-orphans
+	git pull origin prod
+elif [[ $1 = "shell" ]]; then
+	echo "Entering Jira Logger API Shell..."
+	docker exec -it spillman-api sh
+else
+	echo "Unkown or missing command..."
+fi
